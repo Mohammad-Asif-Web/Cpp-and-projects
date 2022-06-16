1ac85c91872ebf746cd79c895145d29ab7612f24
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2022-06-16 09:34:57 +0600

    new files added, old files modified

---
+++ data_structure_algorithm/ps01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+    printf("Enter first number\n");
+    scanf("%d", &a);
+    printf("Enter second number\n");
+    scanf("%d", &b);
+    printf("The sum is %d\n", a + b);
+    return 0;
+}
